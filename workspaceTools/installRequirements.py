7573d2f6ddea884df2e39d46074e5bf9bdbc6c5a
Tyler <hereto.tyler@gmail.com>
2022-11-22 00:39:06 -0700

    tweaks

--- workspaceTools/installRequirements.py
+++ workspaceTools/installRequirements.py
@@ -1,11 +1,13 @@
 import subprocess
 import os
+import sys
 
 if not os.path.isfile("requirements.txt"):
     print("requirements.txt not found")
     exit(1)
 
-cmd = "pip install -r requirements.txt"
+
+cmd = "python -m pip install -r requirements.txt"  
 try:
     subprocess.check_call(
         cmd, 
