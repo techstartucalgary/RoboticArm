6b7e8cb43abf5051fe92f3383be34b0a65a0a556
Archibald <archibaldchain@gmail.com>
2023-02-22 15:10:24 -0700

    fixed some error for steps

--- gym_env/test_kenova_pick_place.py
+++ gym_env/test_kenova_pick_place.py
@@ -3,20 +3,20 @@ from gym.wrappers.rescale_action import RescaleAction
 
 if __name__ == "__main__":
     kpa = KenovaPickAndPlace()
-    
+
     print(kpa.action_space)
     print(kpa.observation_space)
 
     kpa = RescaleAction(kpa, -1, 1)
     print(kpa.action_space)
-    obs = kpa.reset()
-    print(obs)
+    # obs = kpa.reset()
+    # print(obs)
     action_space = kpa.action_space
-    
+
     n_steps = 100
-    # for i in range(n_steps):
-    #     kpa.action_space
-
-
-
-
+    for i in range(n_steps):
+        action = kpa.action_space.sample()
+        print(action)
+        obs, reward, is_success, _, info = kpa.step(action)
+        # kpa.render()
+        print(obs, reward, is_success, _, info)
